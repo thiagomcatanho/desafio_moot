cbd699c2e5d8d95cf8a816158a51baddf8a02d19
Thiago Catanho <thiagomcatanho@gmail.com>
2025-06-20 14:37:16 -0300

    feat: criação das migrations

---
+++ database/migrations/2025_06_17_212457_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome')->unique();
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('marca_id');
+
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->foreign('marca_id')->references('id')->on('marcas');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
