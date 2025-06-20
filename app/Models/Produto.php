c86ea9213a73625d37fa63d4a24e184d70533681
Thiago Catanho <thiagomcatanho@gmail.com>
2025-06-20 14:38:07 -0300

    feat: criação dos models

---
+++ app/Models/Produto.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    public function marca(): BelongsTo
+    {
+        return $this->belongsTo(Marca::class);
+    }
+
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+}
